6daa80c87b1694058426287d677917b2ead365f0
Lubna Febin <lubnafebin13@gmail.com>
2025-12-03 20:11:24 +0530

    Add initial project files for Travel Journal static application

---
+++ travel-journal-static/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="global.png" alt="logo" />
+      <h1>My Travel Journal</h1>
+    </header>
+  );
+}
---
+++ travel-journal-static/data.js
@@ -0,0 +1,41 @@
+[
+  {
+    id: 1,
+    img: {
+      src: "https://images.unsplash.com/photo-1610375229632-c7158c35a537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnQlMjBmdWppfGVufDB8fDB8fHww",
+      alt: "Mount Fuji",
+    },
+    title: "Mount Fuji",
+    country: "Japan",
+    googleMapUrl:
+      "https://www.google.com/maps/about/behind-the-scenes/streetview/treks/mount-fuji/",
+    dates: "12 Feb,2021 - 24 Feb,2021",
+    text: "It is the highest mountain in Japan, the second-highest volcano on any Asian island after Mount Kerinci in Indonesia, and the seventh-highest peak on an island globally.",
+  },
+  {
+    id: 2,
+    img: {
+      src: "https://images.unsplash.com/photo-1610375229632-c7158c35a537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnQlMjBmdWppfGVufDB8fDB8fHww",
+      alt: "Mount Fuji",
+    },
+    title: "Mount Fuji",
+    country: "Japan",
+    googleMapUrl:
+      "https://www.google.com/maps/about/behind-the-scenes/streetview/treks/mount-fuji/",
+    dates: "12 Feb,2021 - 24 Feb,2021",
+    text: "It is the highest mountain in Japan, the second-highest volcano on any Asian island after Mount Kerinci in Indonesia, and the seventh-highest peak on an island globally.",
+  },
+  {
+    id: 3,
+    img: {
+      src: "https://images.unsplash.com/photo-1610375229632-c7158c35a537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnQlMjBmdWppfGVufDB8fDB8fHww",
+      alt: "Mount Fuji",
+    },
+    title: "Mount Fuji",
+    country: "Japan",
+    googleMapUrl:
+      "https://www.google.com/maps/about/behind-the-scenes/streetview/treks/mount-fuji/",
+    dates: "12 Feb,2021 - 24 Feb,2021",
+    text: "It is the highest mountain in Japan, the second-highest volcano on any Asian island after Mount Kerinci in Indonesia, and the seventh-highest peak on an island globally.",
+  },
+];

14964b6b1d3447e02d2a23929ca197910d0b5e6e
Lubna Febin <lubnafebin13@gmail.com>
2025-12-04 19:15:13 +0530

    Fix image source in Header component

--- travel-journal-static/components/Header.jsx
+++ travel-journal-static/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <img src="global.png" alt="logo" />
+      <img src="globe.png" alt="logo" />
       <h1>My Travel Journal</h1>
     </header>
   );

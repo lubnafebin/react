2d2a05c954942ae817bab8e60f51e674cff7aef4
Lubna Febin <lubnafebin13@gmail.com>
2025-12-05 19:53:03 +0530

    Update Main component to accept props for dynamic rendering of content

--- travel-journal-static/components/Main.jsx
+++ travel-journal-static/components/Main.jsx
@@ -1,16 +1,16 @@
-export default function Main() {
+export default function Main(props) {
   return (
     <article className="journal">
       <div className="img-container">
-        <img className="main-img" src="exp.png" alt="travel" />
+        <img className="main-img" src={props.img} alt="travel" />
       </div>
       <div className="info-container">
         <img src="marker.png" alt="marker" className="marker" />
-        <span className="country">Japan</span>
-        <a href="">View On Google Map</a>
-        <h2 className="title"></h2>
-        <p className="date"></p>
-        <p className="entry-text"></p>
+        <span className="country">{props.title}</span>
+        <a href="">{props.googleMapUrl}</a>
+        <h2 className="title">{props.country}</h2>
+        <p className="date">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
     </article>
   );

9a90b88174e208be916a953faf97e85bdf780d9c
Lubna Febin <lubnafebin13@gmail.com>
2025-12-05 19:52:54 +0530

    Refactor App component to dynamically render Main components from data

--- travel-journal-static/App.jsx
+++ travel-journal-static/App.jsx
@@ -1,10 +1,23 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import data from "./data";
 export default function App() {
+  const mainElements = data.map((item) => {
+    return (
+      <Main
+        img={item.img}
+        title={item.title}
+        country={item.country}
+        googleMapUrl={item.googleMapUrl}
+        dates={item.dates}
+        text={item.text}
+      />
+    );
+  });
   return (
     <>
       <Header />
-      <Main />
+      {mainElements}
     </>
   );
 }

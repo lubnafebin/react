d90ff43b63cb8ba8989e61e9b8995f168586cc58
Lubna Febin <lubnafebin13@gmail.com>
2025-12-08 19:30:14 +0530

    feat: add Header component with logo and title

---
+++ chef-claude/src/Header.jsx
@@ -0,0 +1,9 @@
+import icon from "./assets/logo.jpg";
+export const Header = () => {
+  return (
+    <header>
+      <img src={icon} alt="logo" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};

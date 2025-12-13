41d326cc81be310b49b98eca58aad51cfe3011c7
Lubna Febin <lubnafebin13@gmail.com>
2025-12-13 21:09:40 +0530

    feat: add ClaudeRecipe component for displaying recommended recipes

---
+++ chef-claude/src/components/ClaudeRecipe.jsx
@@ -0,0 +1,23 @@
+export const ClaudeRecipe = () => {
+  return (
+    <section>
+      <h2>Chef claude recommends:</h2>
+      <article>
+        <p></p>
+        <h3></h3>
+        <strong>Ingredients</strong>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <strong>Instructions</strong>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </article>
+    </section>
+  );
+};

9bf8e0230e6f865d387bd8f53756e32f6d99c416
Lubna Febin <lubnafebin13@gmail.com>
2025-12-16 20:30:12 +0530

    wrapped with react markdown

--- chef-claude/src/components/ClaudeRecipe.jsx
+++ chef-claude/src/components/ClaudeRecipe.jsx
@@ -1,4 +1,9 @@
 import ReactMarkdown from "react-markdown";
 export const ClaudeRecipe = (props) => {
-  return <section>{props.recipe}</section>;
+  return (
+    <section className="suggested-recipe-container" aria-live="polite">
+      <h2>AI Recommends:</h2>
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
+    </section>
+  );
 };

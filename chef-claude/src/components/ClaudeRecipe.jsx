983b36346b65d68180fee7252ce791de92fbb857
Lubna Febin <lubnafebin13@gmail.com>
2025-12-16 15:30:40 +0530

    refactor: simplify ClaudeRecipe component to render recipe prop directly

--- chef-claude/src/components/ClaudeRecipe.jsx
+++ chef-claude/src/components/ClaudeRecipe.jsx
@@ -1,23 +1,4 @@
-export const ClaudeRecipe = () => {
-  return (
-    <section>
-      <h2>Chef claude recommends:</h2>
-      <article>
-        <p></p>
-        <h3></h3>
-        <strong>Ingredients</strong>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-        <strong>Instructions</strong>
-        <ol>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ol>
-      </article>
-    </section>
-  );
+import ReactMarkdown from "react-markdown";
+export const ClaudeRecipe = (props) => {
+  return <section>{props.recipe}</section>;
 };

9fbbb46aa3239871604f9081ae78f70071fcc58a
Lubna Febin <lubnafebin13@gmail.com>
2025-12-18 19:57:11 +0530

    added header section

---
+++ assembly-endgame/src/components/AssemblyEndgame.jsx
@@ -0,0 +1,13 @@
+export const AssemblyEndgame = () => {
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+    </main>
+  );
+};

b0ef46b1bf86bd6873a61f8883f5a3b40c5dc28b
Lubna Febin <lubnafebin13@gmail.com>
2025-12-19 16:15:39 +0530

    added game status section and language display section

--- assembly-endgame/src/components/AssemblyEndgame.jsx
+++ assembly-endgame/src/components/AssemblyEndgame.jsx
@@ -1,4 +1,59 @@
 export const AssemblyEndgame = () => {
+  const languages = [
+    {
+      name: "HTML",
+      backgroundColor: "#E2680F",
+      color: "#F9F4DA",
+    },
+    {
+      name: "CSS",
+      backgroundColor: "#328AF1",
+      color: "#F9F4DA",
+    },
+    {
+      name: "Javascript",
+      backgroundColor: "#F4EB13",
+      color: "#1E1E1E",
+    },
+    {
+      name: "React",
+      backgroundColor: "#2ED3E9",
+      color: "#1E1E1E",
+    },
+    {
+      name: "TypeScript",
+      backgroundColor: "#E2680F",
+      color: "#F9F4DA",
+    },
+    {
+      name: "Nodejs",
+      backgroundColor: "#28bb15ff",
+      color: "#F9F4DA",
+    },
+    {
+      name: "Python",
+      backgroundColor: "#e2c90fff",
+      color: "#1E1E1E",
+    },
+    {
+      name: "Ruby",
+      backgroundColor: "#e22f0fff",
+      color: "#F9F4DA",
+    },
+  ];
+
+  const langElement = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
   return (
     <main>
       <header>
@@ -8,6 +63,11 @@ export const AssemblyEndgame = () => {
           from Assembly!
         </p>
       </header>
+      <section className="game-status">
+        <h2>You Win🎉</h2>
+        <p>Good Job🎉</p>
+      </section>
+      <section className="lang-chips">{langElement}</section>
     </main>
   );
 };

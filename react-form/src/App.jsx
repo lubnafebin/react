a94b3e956b2e5972b4dc3f692cca1aa89ed8e207
Lubna Febin <lubnafebin13@gmail.com>
2026-01-14 20:34:50 +0530

    implement basic counter functionality with button and card component

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,6 +1,19 @@
+import { useCallback, useState } from "react";
+import Card from "./Card";
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = useCallback(() => {
+    console.log("clicked");
+  }, []);
+  return (
+    <div className="main">
+      <h2>Parent Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <Card onClick={handleClick} />
+    </div>
+  );
 }
 
 export default App;
